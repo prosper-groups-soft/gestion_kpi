511868c49af5a86a502c53dc63999040b0ac9928
prosper-groups-soft <108903895+prosper-groups-soft@users.noreply.github.com>
2025-08-21 11:22:23 +0200

    Update wsgi.py
    Change for static files

--- GestionKPI/wsgi.py
+++ GestionKPI/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+from whitenoise import WhiteNoise
+from django.core.wsgi import get_wsgi_application
 import os
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GestionKPI.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GestionKPI.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'staticfiles'))
